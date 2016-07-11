44fc5efdea1f1fe4dc96ef66a8533d8457a55589
eggyo <eggyoyo4859@gmail.com>
2016-07-11 14:21:17 +0700

    v.1.0.2

--- cloud/main.js
+++ cloud/main.js
@@ -4,13 +4,11 @@ Parse.Cloud.define('hello', function(req, res) {
 });
 
 Parse.Cloud.define('getReplyMsg', function(request, response) {
-
                    var MSG = Parse.Object.extend("Message");
                    var query = new Parse.Query(MSG);
                    var msgFromUser = request.params.msg;
                    console.log("request:"+request.params["msg"]);
                    console.log("msg from user:"+msgFromUser);
-
                    query.equalTo("msg", msgFromUser);
                    query.find({
                              success: function(msgResponse) {
@@ -18,24 +16,19 @@ Parse.Cloud.define('getReplyMsg', function(request, response) {
                              contents = msgResponse[0].get("replyMsg");
                              console.log("msgResponse:"+msgResponse);
                              console.log("contents:"+contents);
-
                              var replyCount = contents.length;
                              console.log("replyCount:"+replyCount);
-
                              if (replyCount == 0) {
-                               //response.success({"msg":msgFromUser,"replyMsg":""});
+                               response.success({"msg":msgFromUser,"replyMsg":""});
                                console.log("resultReplyMsg:"+"0");
-
                              }else {
                                var randomIndex = Math.floor((Math.random() * replyCount) + 0);
                                console.log("randomIndex:"+randomIndex);
                                var resultReplyMsg = contents[randomIndex].toString();
-                               //response.success({"msg":msgFromUser,"replyMsg":resultReplyMsg});
+                               response.success({"msg":msgFromUser,"replyMsg":resultReplyMsg});
                                console.log("resultReplyMsg:"+resultReplyMsg);
-
                              }
-                             response.success(msgResponse);
-
+                             //response.success(msgResponse);
                              },
                              error: function() {
                              response.error("get replyMsg failed");
