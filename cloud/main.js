5bb7f287cb4461cc2e8ea0b98a3e152184ead456
eggyo <eggyoyo4859@gmail.com>
2016-07-11 11:44:46 +0700

    v.1.0.2

--- cloud/main.js
+++ cloud/main.js
@@ -8,11 +8,15 @@ Parse.Cloud.define('getReplyMsg', function(request, response) {
                    var MSG = Parse.Object.extend("Message");
                    var query = new Parse.Query(MSG);
                    var msgFromUser = request.params.msg;
+                   console.log("msg from user:"+msgFromUser);
+
                    query.equalTo("msg", msgFromUser);
                    query.find({
                              success: function(msgResponse) {
                              var contents = [];
                              contents = msgResponse.get("replyMsg").toString();
+                             console.log("msgResponse:"+msgResponse"\nconntent:"+contents);
+
                              var replyCount = contents.length;
                              if (replyCount == 0) {
                                response.success({"msg":msgFromUser,"replyMsg":""});
