d6a5811979d8a099d22b45a2f348066ac45d9f62
eggyo <eggyoyo4859@gmail.com>
2017-07-10 20:21:32 +0700

    v.1

--- cloud/main.js
+++ cloud/main.js
@@ -6,103 +6,158 @@ Parse.Cloud.define('hello', function(req, res) {
 
 Parse.Cloud.define('testMsg', function(req, res) {
   var msgFromUser = req.params.msg;
-  console.log("msg from user:"+msgFromUser);
-  res.success({"msg":msgFromUser,"replyMsg":"FUCK"});
+  console.log("msg from user:" + msgFromUser);
+  res.success({
+    "msg": msgFromUser,
+    "replyMsg": "FUCK"
+  });
 });
 
 Parse.Cloud.define('getReplyMsg', function(request, response) {
-                   var MSG = Parse.Object.extend("Message");
-                   var query = new Parse.Query(MSG);
-                   var msgFromUser = request.params.msg;
-                   console.log("request:"+request.params["msg"]);
-                   console.log("msg from user:"+msgFromUser);
-                   if (msgFromUser == null) {
-                     response.error("request null values");
-                   }else {
-                     query.equalTo("msg", msgFromUser);
-                     query.limit(appQueryLimit);
-                     query.find({
-                               success: function(msgResponse) {
-                               var contents = [];
-                               if (msgResponse.length == 0) {
-                                 response.success({"msg":msgFromUser,"replyMsg":""});
-                               }else {
-                                 contents = msgResponse[0].get("replyMsg");
-                                 console.log("msgResponse:"+msgResponse);
-                                 console.log("contents:"+contents);
-                                 var replyCount = contents.length;
-                                 console.log("replyCount:"+replyCount);
-                                 if (replyCount == 0) {
-                                   response.success({"msg":msgFromUser,"replyMsg":""});
-                                   console.log("resultReplyMsg:"+"0");
-                                 }else {
-                                   var randomIndex = Math.floor((Math.random() * replyCount) + 0);
-                                   console.log("randomIndex:"+randomIndex);
-                                   var resultReplyMsg = contents[randomIndex].toString();
-                                   response.success({"msg":msgFromUser,"replyMsg":resultReplyMsg});
-                                   console.log("resultReplyMsg:"+resultReplyMsg);
-                                 }
-                               }
-                               //response.success(msgResponse);
-                               },
-                               error: function() {
-                               response.error("get replyMsg failed");
-                               }
-                             });
-                   }
+  var MSG = Parse.Object.extend("Message");
+  var query = new Parse.Query(MSG);
+  var msgFromUser = request.params.msg;
+  console.log("request:" + request.params["msg"]);
+  console.log("msg from user:" + msgFromUser);
+  if (msgFromUser == null) {
+    response.error("request null values");
+  } else {
+    query.equalTo("msg", msgFromUser);
+    query.limit(appQueryLimit);
+    query.find({
+      success: function(msgResponse) {
+        var contents = [];
+        if (msgResponse.length == 0) {
+          response.success({
+            "msg": msgFromUser,
+            "replyMsg": ""
+          });
+        } else {
+          contents = msgResponse[0].get("replyMsg");
+          console.log("msgResponse:" + msgResponse);
+          console.log("contents:" + contents);
+          var replyCount = contents.length;
+          console.log("replyCount:" + replyCount);
+          if (replyCount == 0) {
+            response.success({
+              "msg": msgFromUser,
+              "replyMsg": ""
+            });
+            console.log("resultReplyMsg:" + "0");
+          } else {
+            var randomIndex = Math.floor((Math.random() * replyCount) + 0);
+            console.log("randomIndex:" + randomIndex);
+            var resultReplyMsg = contents[randomIndex].toString();
+            response.success({
+              "msg": msgFromUser,
+              "replyMsg": resultReplyMsg
+            });
+            console.log("resultReplyMsg:" + resultReplyMsg);
+          }
+        }
+        //response.success(msgResponse);
+      },
+      error: function() {
+        response.error("get replyMsg failed");
+      }
+    });
+  }
 });
 
 Parse.Cloud.define('botTraining', function(request, response) {
-                   var MSG = Parse.Object.extend("Message");
-                   var msgFromUser = request.params.msg;
-                   var replyMsgFromUser = request.params.replyMsg;
-                   console.log("msg from user:"+msgFromUser+"\nreplyMsgFromUser:"+replyMsgFromUser);
-                   if (replyMsgFromUser == null || msgFromUser == null) {
-                     response.error("request null values");
-                   }else {
-                     var query = new Parse.Query(MSG);
-                     query.containedIn("msg", msgFromUser);
-                     query.limit(appQueryLimit);
-                     query.find({
-                       success: function(msgResponse) {
-                       var contents = [];
-                       if (msgResponse.length == 0) {
-                         // add new msg
-                         var msgOBJ = new MSG();
-                         msgOBJ.set("msg",msgFromUser);
-                         msgOBJ.set("replyMsg",replyMsgFromUser);
-                         msgOBJ.save(null, {
-                                     success: function(success) {
-                                     response.success({"msg":msgFromUser,"replyMsg":replyMsgFromUser});
-                                     },
-                                     error: function(error) {
-                                     response.error("save failed : "+error.code);
-                                     }
-                                     });
-                       }else {
-                         // put another reply
-                         var msgOBJ = new MSG();
-                         msgOBJ = msgResponse[0];
-                         for(var i=0;i < msgFromUser.length;i++){
-                            msgOBJ.addUnique("msg",msgFromUser[i]);
-                         }
-                         for(var i=0;i < replyMsgFromUser.length;i++){
-                            msgOBJ.addUnique("replyMsg",replyMsgFromUser[i]);
-                         }
-                         msgOBJ.save(null, {
-                                     success: function(success) {
-                                       response.success({"msg":msgFromUser,"replyMsg":replyMsgFromUser});
-                                     },
-                                     error: function(error) {
-                                     response.error("save failed : "+error.code);
-                                     }
-                                     });
-                       }
-                       //response.success(msgResponse);
-                       },
-                       error: function() {
-                       response.error("get replyMsg failed");
-                       }
-                     });
-                   }// end else
+  var MSG = Parse.Object.extend("Message");
+  var msgFromUser = request.params.msg;
+  var replyMsgFromUser = request.params.replyMsg;
+  console.log("msg from user:" + msgFromUser + "\nreplyMsgFromUser:" + replyMsgFromUser);
+  if (replyMsgFromUser == null || msgFromUser == null) {
+    response.error("request null values");
+  } else {
+    var query = new Parse.Query(MSG);
+    query.containedIn("msg", msgFromUser);
+    query.limit(appQueryLimit);
+    query.find({
+      success: function(msgResponse) {
+        var contents = [];
+        if (msgResponse.length == 0) {
+          // add new msg
+          var msgOBJ = new MSG();
+          msgOBJ.set("msg", msgFromUser);
+          msgOBJ.set("replyMsg", replyMsgFromUser);
+          msgOBJ.save(null, {
+            success: function(success) {
+              response.success({
+                "msg": msgFromUser,
+                "replyMsg": replyMsgFromUser
+              });
+            },
+            error: function(error) {
+              response.error("save failed : " + error.code);
+            }
+          });
+        } else {
+          // put another reply
+          var msgOBJ = new MSG();
+          msgOBJ = msgResponse[0];
+          for (var i = 0; i < msgFromUser.length; i++) {
+            msgOBJ.addUnique("msg", msgFromUser[i]);
+          }
+          for (var i = 0; i < replyMsgFromUser.length; i++) {
+            msgOBJ.addUnique("replyMsg", replyMsgFromUser[i]);
+          }
+          msgOBJ.save(null, {
+            success: function(success) {
+              response.success({
+                "msg": msgFromUser,
+                "replyMsg": replyMsgFromUser
+              });
+            },
+            error: function(error) {
+              response.error("save failed : " + error.code);
+            }
+          });
+        }
+        //response.success(msgResponse);
+      },
+      error: function() {
+        response.error("get replyMsg failed");
+      }
+    });
+  } // end else
+});
+
+Parse.Cloud.define('createUnknowMsg', function(request, response) {
+  var MSG = Parse.Object.extend("UnknownMessage");
+  var msgFromUser = request.params.msg;
+  console.log("msg from user:" + msgFromUser);
+  if (msgFromUser == null) {
+    response.error("request null values");
+  } else {
+    var query = new Parse.Query(MSG);
+    query.containedIn("msg", msgFromUser);
+    query.limit(appQueryLimit);
+    query.find({
+      success: function(msgResponse) {
+        var contents = [];
+        if (msgResponse.length == 0) {
+          // add new msg
+          var msgOBJ = new MSG();
+          msgOBJ.set("msg", msgFromUser);
+          msgOBJ.save(null, {
+            success: function(success) {
+              response.success({
+                "msg": msgFromUser
+              });
+            },
+            error: function(error) {
+              response.error("save failed : " + error.code);
+            }
+          });
+        }
+        //response.success(msgResponse);
+      },
+      error: function() {
+        response.error("find failed");
+      }
+    });
+  } // end else
 });
