5c33e5f8c24d60de82252acff3bd0b029aa3011b
eggyo <eggyoyo4859@gmail.com>
2017-07-12 12:26:35 +0700

    v.1

--- cloud/main.js
+++ cloud/main.js
@@ -161,3 +161,54 @@ Parse.Cloud.define('createUnknowMsg', function(request, response) {
     });
   } // end else
 });
+
+Parse.Cloud.define('findBestReplyMsg', function(request, response) {
+  var MSG = Parse.Object.extend("Message");
+  var query = new Parse.Query(MSG);
+  var msgFromUser = request.params.msg;
+  console.log("request:" + request.params["msg"]);
+  console.log("msg from user:" + msgFromUser);
+  if (msgFromUser == null) {
+    response.error("request null values");
+  } else {
+    query.contains("msg", msgFromUser);
+    query.limit(appQueryLimit);
+    query.find({
+      success: function(msgResponse) {
+        var contents = [];
+        if (msgResponse.length == 0) {
+          response.success({
+            "msg": msgFromUser,
+            "replyMsg": ""
+          });
+        } else {
+          contents = msgResponse[0].get("replyMsg");
+          console.log("msgResponse:" + msgResponse);
+          console.log("contents:" + contents);
+          var replyCount = contents.length;
+          console.log("replyCount:" + replyCount);
+          if (replyCount == 0) {
+            response.success({
+              "msg": msgFromUser,
+              "replyMsg": ""
+            });
+            console.log("resultReplyMsg:" + "0");
+          } else {
+            var randomIndex = Math.floor((Math.random() * replyCount) + 0);
+            console.log("randomIndex:" + randomIndex);
+            var resultReplyMsg = contents[randomIndex].toString();
+            response.success({
+              "msg": msgFromUser,
+              "replyMsg": resultReplyMsg
+            });
+            console.log("resultReplyMsg:" + resultReplyMsg);
+          }
+        }
+        //response.success(msgResponse);
+      },
+      error: function() {
+        response.error("get replyMsg failed");
+      }
+    });
+  }
+});
