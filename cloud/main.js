34dfb680530bb90d702d438dc9e330545ed6ecfe
eggyo <eggyoyo4859@gmail.com>
2017-07-12 15:08:26 +0700

    v.1

--- cloud/main.js
+++ cloud/main.js
@@ -256,7 +256,7 @@ Parse.Cloud.define("findBestReplyMsgFromCharSet", function(request, response) {
   if (msgFromUser == null) {
     response.error("request null values");
   } else {
-    query.containedIn("charSet", arr);
+    query.containsAll("charSet", arr);
     query.limit(appQueryLimit);
     query.find({
       success: function(msgResponse) {
